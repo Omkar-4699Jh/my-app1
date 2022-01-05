f618692a3004202c306cecf9ad2dcd888fb4277d
Omkar-4699Jh <omkargadakh4699@gmail.com>
2022-01-05 15:38:38 +0530

    4

--- src/App.js
+++ src/App.js
@@ -9,32 +9,17 @@ export default function App() {
 }
 
 function MyComponent() {
-  const id1 = 100;
-
-  const [cardCss, setCardCss] = useState("bg-info text-light fs-3 p-4 mb-1");
-  const list = [1, 1, 1, 1, 1, 1];
-  const Primary = () => setCardCss("bg-info text-light fs-3 p-4 mb-1");
-  const secondary = () => setCardCss("bg-light text-dark fs-3 p-4 mb-1");
+  const id = 1;
+  const msg = "hello,";
+  const btnmsg = "how are you!!!";
+  const hello = () => {
+    console.log("hii hello are u there");
+    alert("thank you");
+  };
   return (
-    <div>
-      <input
-        type="button"
-        value="Primary"
-        className="btn btn-primary btn-sm"
-        onClick={Primary}
-      />
-      <input
-        type="button"
-        value="secondary"
-        className="btn btn-secondary btn-sm"
-        onClick={secondary}
-      />
-      {list.map(() => (
-        <div className={cardCss}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus,
-          illo amet officiis iure alias explicabo unde odio
-        </div>
-      ))}
+    <div id={id}>
+      {msg}
+      <input type="button" value={btnmsg} onClick={hello} />
     </div>
   );
 }
