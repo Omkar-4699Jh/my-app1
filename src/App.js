ccd15f3b2e6289048e54de849ef75ed189d354db
Omkar-4699Jh <omkargadakh4699@gmail.com>
2022-01-06 23:50:44 +0530

    6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function App() {
   return (
     <>
-      <MyComponent></MyComponent>
+      <Mycomponent></Mycomponent>
     </>
   );
 }
 
-function MyComponent() {
-  let counter = 1;
-  const increment = () => {
-    console.log("increment called", counter++);
+function Mycomponent() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [list, setList] = useState([]);
+  const UserChange = (e) => {
+    setUsername(e.target.value);
   };
+  const PassChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const addUser = async () => {
+    if (username == "" || password == "") {
+      alert("not valid");
+      return;
+    }
+    //alert("");
+    console.log(username, password);
+    const url = "http://localhost:4000/add-user";
+    const data = {
+      username: username,
+      password: password,
+    };
+    await axios.post(url, data);
+
+    const newlist = [data, ...list];
+    setList(newlist);
+    setUsername("");
+    setPassword("");
+  };
+  const getUser = async () => {
+    const url = "http://localhost:4000/users";
+
+    const result = await axios.get(url);
+    const list = result.data;
+    const newList = [...list];
+    setList(newList);
+  };
+  useEffect(() => getUser(), []);
   return (
     <div>
-      <h1>Counter application</h1>
-      <div>Like{counter}</div>
-      <input type="button" value="Like me" onClick={increment} />
+      <h1
+        className="bg-info text-light 
+      "
+      >
+        User Registration
+      </h1>
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter Username"
+          value={username}
+          onChange={UserChange}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter password"
+          value={password}
+          onChange={PassChange}
+        />
+      </div>
+      <div>
+        <input type="button" value="Register" onClick={addUser} />
+        <input type="button" value="get info" onClick={getUser} />
+      </div>
+      <h1>User List</h1>
+      {list.map((item, index) => (
+        <div className="text-center" key={index}>
+          {item.username} {item.password}
+        </div>
+      ))}
     </div>
   );
 }
